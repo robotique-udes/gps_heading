7727810a99b1e73033afb6f7fc175b66caa13981
Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
2022-04-23 16:06:22 -0400

    Simplified algorithm, reusing imu covariance

--- src/gps_heading.py
+++ src/gps_heading.py
@@ -3,25 +3,26 @@
 import rospy
 from sensor_msgs.msg import Imu, NavSatFix
 from geometry_msgs.msg import Quaternion, QuaternionStamped
-from math import sqrt, atan2, pi
+from math import pi, degrees, radians
 from tf import TransformListener
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
 from collections import deque
 from threading import Lock
 from geographiclib.geodesic import Geodesic
+from copy import deepcopy
 
 class GpsHeading():
     def __init__(self):
         # ROS stuff
         rospy.init_node("gps_heading", anonymous = True)
-        self.gps_sub = rospy.Subscriber('/fix', NavSatFix, self.gpsCB)
-        self.imu_sub = rospy.Subscriber('/imu/data', Imu, self.imuCB)
+        self.gps_sub = rospy.Subscriber('/fix', NavSatFix, self.gpsCB, queue_size=1)
+        self.imu_sub = rospy.Subscriber('/imu/data', Imu, self.imuCB, queue_size=1)
         self.heading_pub = rospy.Publisher('/gps_heading', Imu, queue_size=1)  # TODO: change to pose?
         self.tf = TransformListener()
 
         # Parameters
         self.publishing_rate = rospy.get_param("~publishing_rate", 10)  # Hz
-        self.min_distance = rospy.get_param("~min_distance", 3)  # TODO: ROS parameter
+        self.min_distance = rospy.get_param("~min_distance", 5)  # TODO: ROS parameter
         self.base_frame = rospy.get_param("~base_frame", "base_link")
         self.gps_positions_queue_length = rospy.get_param("~gps_positions_queue_length", 5)  # Newest positions are added on the left, oldest positions are on the right
         self.orientation_covariance = [2.6030820491461885e-07, 0.0, 0.0, \
@@ -34,7 +35,7 @@ class GpsHeading():
         self.got_first_gps_heading = False
         self.previous_gps_heading = 0
         self.gps_heading_has_converged = False  # Only switch to GPS heading once it has converged
-        self.gps_heading_epsilon = rospy.get_param("~gps_heading_epsilon", 0.0523599)  # Maximum difference in radians between the current gps heading and previous gps heading to consider the heading converged
+        self.gps_heading_epsilon = rospy.get_param("~gps_heading_epsilon", 0.0174533)  # Maximum difference in radians between the current gps heading and previous gps heading to consider the heading converged
         self.nb_of_heading_disagreements = 0  # Number of converged GPS headings that have been calculated since the IMU heading was last corrected
 
         # IMU variable
@@ -45,26 +46,7 @@ class GpsHeading():
         self.max_nb_of_heading_disagreements = rospy.get_param("~max_nb_of_heading_disagreements", 3)  # Number of times a disagreement between the IMU heading and the GPS heading will be tolerated before a correction is forced.
 
         # Other variables
-        self.heading_msg = Imu()
         self.mutex = Lock()  # Mutex is used because the two callbacks run in their own thread but they use shared data
-
-        # Initialization process
-        self.heading_msg.header.frame_id = self.base_frame
-        self.heading_msg.orientation_covariance = self.orientation_covariance
-
-    def run(self):
-        try:
-            r = rospy.Rate(self.publishing_rate)
-            while not rospy.is_shutdown():
-                self.mutex.acquire()
-                try:
-                    self.heading_msg.header.stamp = rospy.Time.now()
-                    self.heading_pub.publish(self.heading_msg)
-                finally:
-                    self.mutex.release()
-                r.sleep()
-        except rospy.exceptions.ROSInterruptException:
-            pass
             
     def imuCB(self, msg):
         quat = QuaternionStamped()
@@ -85,23 +67,25 @@ class GpsHeading():
         previous_imu_euler_orientation = euler_from_quaternion(previous_imu_quaternion_orientation)
         current_imu_euler_orientation = euler_from_quaternion(current_imu_quaternion_orientation)
 
-        self.relative_yaw += wrap_angle_pi(current_imu_euler_orientation[2] - previous_imu_euler_orientation[2])
-        self.relative_yaw = wrap_angle_pi(self.relative_yaw)
-        absolute_yaw = wrap_angle_pi(self.relative_yaw + self.latest_gps_heading)
+        self.mutex.acquire()
+        try:
+            self.relative_yaw += wrap_angle_pi(current_imu_euler_orientation[2] - previous_imu_euler_orientation[2])
+            self.relative_yaw = wrap_angle_pi(self.relative_yaw)
+            absolute_yaw = wrap_angle_pi(self.relative_yaw + self.latest_gps_heading)
+            # print("current heading: %.2f" % degrees(absolute_yaw))
+        finally:
+            self.mutex.release()
 
         absolute_quaternion_orientation = quaternion_from_euler(current_imu_euler_orientation[0], current_imu_euler_orientation[1], absolute_yaw)
 
-        self.mutex.acquire()
-        try:
-            self.latest_imu_euler_orientation = current_imu_euler_orientation
-            self.heading_msg.orientation.x = absolute_quaternion_orientation[0]
-            self.heading_msg.orientation.y = absolute_quaternion_orientation[1]
-            self.heading_msg.orientation.z = absolute_quaternion_orientation[2]
-            self.heading_msg.orientation.w = absolute_quaternion_orientation[3]
-        finally:
-            self.mutex.release()
-        # Rest of IMU msg is left empty since we only care about the orientation
+        heading_msg = deepcopy(msg)
+        heading_msg.orientation.x = absolute_quaternion_orientation[0]
+        heading_msg.orientation.y = absolute_quaternion_orientation[1]
+        heading_msg.orientation.z = absolute_quaternion_orientation[2]
+        heading_msg.orientation.w = absolute_quaternion_orientation[3]
+        self.heading_pub.publish(heading_msg)
 
+        self.latest_imu_euler_orientation = current_imu_euler_orientation
         self.previous_imu_orientation = transformed_quat.quaternion
 
 
@@ -116,10 +100,10 @@ class GpsHeading():
         for position in self.gps_position_queue:
             # Starting from the most recent previous position, find a previous position that is
             # farther than the minimum distance threshold
-            distance = calculate_distance(msg.latitude, msg.longitude, position[0], position[1])
+            distance = calculate_distance(position, (msg.latitude, msg.longitude))
 
             if distance > self.min_distance:
-                yaw = calculate_bearing(msg.latitude, msg.longitude, position[0], position[1])
+                yaw = calculate_bearing(position, (msg.latitude, msg.longitude))
                 if not self.got_first_gps_heading:
                     rospy.loginfo("Got first gps heading")
                     self.previous_gps_heading = yaw
@@ -130,24 +114,9 @@ class GpsHeading():
                     rospy.loginfo("GPS heading converged")
                     self.mutex.acquire()
                     try:
-                        if abs(self.relative_yaw) <= self.gps_heading_epsilon or self.nb_of_heading_disagreements >= self.max_nb_of_heading_disagreements:
-                            rospy.loginfo("Correcting IMU heading with GPS heading")
-                            self.nb_of_heading_disagreements = 0
-                            # Only use/update GPS heading if it has converged (aka the robot is moving in a straight line). Otherwise, use IMU
-                            self.latest_gps_heading = yaw 
-                            quaternion_orientation = quaternion_from_euler(self.latest_imu_euler_orientation[0],  self.latest_imu_euler_orientation[1], \
-                                                                        self.latest_gps_heading)
-
-                            self.heading_msg.orientation.x = quaternion_orientation[0]
-                            self.heading_msg.orientation.y = quaternion_orientation[1]
-                            self.heading_msg.orientation.z = quaternion_orientation[2]
-                            self.heading_msg.orientation.w = quaternion_orientation[3]     
-                            # Rest of IMU msg is left empty since we only care about the orientation
-
-                            self.relative_yaw = 0  # Reset relative yaw since a new gps heading has arrived
-                        else:
-                            self.nb_of_heading_disagreements += 1
-                            rospy.loginfo("Number of heading disagreements: %d" % self.nb_of_heading_disagreements)
+                        self.latest_gps_heading = yaw
+                        self.relative_yaw = 0  # Reset relative yaw since a new gps heading has arrived 
+                        print("GPS heading: %.2f" % degrees(self.latest_gps_heading))
                     finally:
                         self.mutex.release()
                 
@@ -161,10 +130,12 @@ def wrap_angle_pi(angle):
     return (angle + pi) % (2 * pi) - pi
 
 def calculate_bearing(p1, p2):
-    # Returns the bearing from point 1 to point 2 (lat/long). Angle is in degrees, from -180 to 180, with 0 pointing North.
-    # Positive is clockwise and negative is counter clockwise
-    geod = Geodesic.WGS84
-    return geod.Inverse(p1[0], p1[1], p2[0], p2[1])['azi1']
+    # Returns the bearing from point 1 to point 2 (lat/long). 
+    # by default, the geodesic azimuth value is in degrees, from -pi to pi, with 0 pointing North.
+    # Positive is clockwise and negative is counter-clockwise
+    # To match the ROS convention, units have to be in radians, the zero should point east and
+    # positive should be counter-clockwise
+    return wrap_angle_pi(-radians(Geodesic.WGS84.Inverse(p1[0], p1[1], p2[0], p2[1])['azi1']) + pi/2)
 
 def calculate_distance(p1, p2):
     # Returns the distance in meters between two points (lat/long)
@@ -173,13 +144,6 @@ def calculate_distance(p1, p2):
 
 
 if __name__ == '__main__':
-    # gps_heading = GpsHeading()
-    # rospy.loginfo("gps_heading ready")
-    # gps_heading.run()
-
-    geod = Geodesic.WGS84
-    p1 = (45.377886, -71.920422)
-    p2 = (45.377886, -71.920417)
-    g = geod.Inverse(p1[0], p1[1], p2[0], p2[1])
-    azimuth = g['azi1']
-    print(azimuth)
+    gps_heading = GpsHeading()
+    rospy.loginfo("gps_heading ready")
+    rospy.spin()
